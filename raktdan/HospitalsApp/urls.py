f78a78e7481793df29327e6992364ae137eec1c0
Amarjeet5822 <amar.bst5822@gmail.com>
2024-07-07 16:28:24 +0530

    work on styling button and fixed issue

--- raktdan/HospitalsApp/urls.py
+++ raktdan/HospitalsApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ViewHospital,CreateHospital
+from .views import ViewHospital,CreateHospital,Single_ViewHospital,DeleteHospital,UpdateHospital
 
 urlpatterns = [
     path("",ViewHospital.as_view(),name="viewHospital"),
-    path("create/",CreateHospital.as_view(),name = "createHospital")
+    path("create/",CreateHospital.as_view(),name = "createHospital"),
+    path("single_hospital/<int:pk>/",Single_ViewHospital.as_view(),name="singleHospital"),
+    path("update_hospital/<int:pk>/",UpdateHospital.as_view(),name="updateHospital"),
+    path("delete_hospital/<int:pk>/",DeleteHospital.as_view(),name="deleteHospital")
     
 ]
--- raktdan/HospitalsApp/views.py
+++ raktdan/HospitalsApp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView,DetailView
 from .models import HospitalDetail
 # from .models import 
 #Create your views here.
@@ -14,8 +14,25 @@ class ViewHospital(ListView):
     template_name = "HospitalsApp/hospitals_list.html"
     context_object_name = "hospitals"
 
+class Single_ViewHospital(DetailView):
+    model = HospitalDetail
+    template_name = "HospitalsApp/single_hospital.html"
+    context_object_name = "hospital" 
+
 class CreateHospital(CreateView):
     model = HospitalDetail
     template_name = "HospitalsApp/add_hospital.html"
     fields = ["name","email","contact","website","country","state","district","address","pin","latitude","longitude"]
+    success_url = reverse_lazy("viewHospital")
+
+class UpdateHospital(UpdateView):
+    model = HospitalDetail
+    template_name = "HospitalsApp/add_Hospital.html"
+    fields = ["name","email","contact","website","country","state","district","address","pin","latitude","longitude"]
+    success_url = reverse_lazy("viewHospital")
+
+class DeleteHospital(DeleteView):
+    model = HospitalDetail
+    template_name = "HospitalsApp/delete_hospital.html"
+    context_object_name = "hospital"
     success_url = reverse_lazy("viewHospital")
